84f963de9ba2b94bdc368a28398ca4404c82de05
Eleonora <eleonora.racca@cern.ch>
2018-06-12 11:31:27 +0200

    prima versione della classe Vertice

---
+++ Provette/Vertice.cxx
@@ -0,0 +1,61 @@
+/*~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+  ~ Implementazione della classe Vertice                      ~
+  ~ Autori: Racca Eleonora - eleonora.racca288@edu.unito.it ~
+  ~         Sauda Cristina - cristina.sauda@edu.unito.it    ~
+  ~ Ultima modifica: 12/06/2018                             ~
+  ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~*/
+
+#if !defined (__CINT__) || defined (__MAKECINT__)
+#include "Vertice.h"
+#include "Punto.h"
+#include "TMath.h"
+#endif
+
+ClassImp(Vertice)
+Vertice::Vertice(): Punto(),
+   dmMolteplicita(0),
+  // dmID(0),
+   dmRumore(0)
+{}
+
+//Vertice::Vertice(double X,double Y,double Z,int Mult,int IDnumber,double Noiselvl):
+Vertice::Vertice(double X,double Y,double Z,int Molteplicita, double Rumore):
+   Punto(X,Y,Z),
+   dmMolteplicita(Mult),
+   //dmID(ID),
+   dmRumore(Rumore)
+   {}
+
+Vertice::~Vertice() 
+
+// ---------- Getter ----------
+int GetMolteplicita()
+{
+  return dmMolteplicita;
+}
+
+/*int GetID()
+{
+  return dmID;
+}*/
+
+double GetRumore()
+{
+  return dmRumore;
+}
+
+// ---------- Setter ----------
+void Vertice::SetMolteplicita(int Molteplicita) 
+{
+  dmMolteplicita = Molteplicita;
+}
+
+/*void Vertice::SetID(int ID) 
+{
+  dmID = ID;
+}*/
+
+void Vertice::SetRumore(double Rumore) 
+{
+  dmRumore = Rumore;
+}
---
+++ Provette/Vertice.h
@@ -0,0 +1,38 @@
+/*~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+  ~ Dichiarazione della classe Vertice                        ~
+  ~ Autori: Racca Eleonora - eleonora.racca288@edu.unito.it ~
+  ~         Sauda Cristina - cristina.sauda@edu.unito.it    ~
+  ~ Ultima modifica: 12/06/2018                             ~
+  ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~*/
+
+#ifndef VERTICE_H
+#define VERTICE_H
+#include "Punto.h"
+
+class Vertice : public Punto {
+   public:
+      Vertice();
+      //Vertice(double X, double Y, double Z, int Molteplicita, int ID, int Rumore = 0);
+      Vertice(double X, double Y, double Z, int Molteplicita, int Rumore = 0);
+      virtual ~Vertice();
+
+      // ---------- Setters ----------
+      void SetMolteplicita(int Molteplicita);
+      //void SetID(int ID);
+      void SetRumore(double Rumore);
+      
+      // ---------- Getters ----------
+      int GetMolteplicita() const;
+      //int GetID() const;
+      double GetRumore() const;
+      
+
+   private:
+      int dmMolteplicita;   // Numero di tracce da generare
+      //int dmID;       // Etichetta utile
+      double dmRumore;  // Assegna il livello del rumore
+      
+
+   ClassDef(Vertice,1)
+};
+#endif
