f6ccf7371cfc64459dc5377b697ef3f1ffa4be7c
Eleonora <eleonora.racca@cern.ch>
2018-06-12 23:25:23 +0200

    primo commit della classe rivelatore

---
+++ Provette/Rivelatore.cxx
@@ -0,0 +1,7 @@
+/*~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+  ~ Dichiarazione della classe Vertice                        ~
+  ~ Autori: Racca Eleonora - eleonora.racca288@edu.unito.it ~
+  ~         Sauda Cristina - cristina.sauda@edu.unito.it    ~
+  ~ Ultima modifica: 12/06/2018                             ~
+  ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~*/
+
