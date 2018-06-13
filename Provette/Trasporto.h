6ba77704e9448f6a1c86b3f9683c31743a0f0f67
crisauda <cristina.sauda@edu.unito.it>
2018-06-13 12:32:30 +0200

    modifica indentazione

--- Provette/Trasporto.h
+++ Provette/Trasporto.h
@@ -12,50 +12,51 @@
 
 class Trasporto : public TObject {
 
-   public:
-      // ----------- Costruttori -----------
-      Trasporto();
-      Trasporto(double Theta, double Phi, int ID);
+	public:
+  	// ----------- Costruttori -----------
+    Trasporto();
+    Trasporto(double Theta, double Phi, int ID);
       
-      // ----------- Distruttori -----------
-      virtual ~Trasporto();
+    // ----------- Distruttori -----------
+    virtual ~Trasporto();
+		{}
+	
+    // ----------- Setter -----------
+    void SetDirTheta(const double Theta);
+    void SetDirPhi(const double Phi);
+    void SetCosDirx(const double Cx);
+    void SetCosDiry(const double Cy);
+    void SetCosDirz(const double Cz);
+    void SetAllCos(const double Cx, const double Cy, const double Cz);
+    void SetAllAngles(const double Theta, const double Phi);
+    void SetDirectID(const double id);
+    void FlipBit();
+    void Rotate(double Theta, double Phi);
 
-      // ----------- Setter -----------
-      void SetDirTheta(const double Theta);
-      void SetDirPhi(const double Phi);
-      void SetCosDirx(const double Cx);
-      void SetCosDiry(const double Cy);
-      void SetCosDirz(const double Cz);
-      void SetAllCos(const double Cx, const double Cy, const double Cz);
-      void SetAllAngles(const double Theta, const double Phi);
-      void SetDirectID(const double id);
-      void FlipBit();
-      void Rotate(double Theta, double Phi);
+	  // ----------- Getter -----------
+    int    GetDirectID()     const; {return dmID;}
+    double GetDirectTheta()  const; {return dmTheta;}
+    double GetDirectPhi()    const; {return dmPhi;}
+    double GetDirCos1()      const; {return dmCDx;}
+    double GetDirCos2()      const; {return dmCDy;}
+    double GetDirCos3()      const; {return dmCDz;}
+    bool   GetRotStatus()    const; {return dmIsrotated;}
 
-      // ----------- Getter -----------
-      int    GetDirectID()     const; {return dmID;}
-      double GetDirectTheta()  const; {return dmTheta;}
-      double GetDirectPhi()    const; {return dmPhi;}
-      double GetDirCos1()      const; {return dmCDx;}
-      double GetDirCos2()      const; {return dmCDy;}
-      double GetDirCos3()      const; {return dmCDz;}
-      bool   GetRotStatus()    const; {return dmIsrotated;}
+  private:
 
-   private:
+    void UpdateAng();
+    void UpdateDirCos();
 
-      void UpdateAng();
-      void UpdateDirCos();
+    // ----------- Data Member -----------
+    int    dmID;    			//! Numero identificativo
+    double dmTheta;       //! Angolo ϑ
+    double dmPhi;         //! Angolo φ
+    double dmCDx;         //! Coseno Direttore x.
+    double dmCDy;         //! Coseno Direttore y.
+    double dmCDz;         //! Coseno Direttore z.
+    bool   dmIsrotated;   //! Debug flag.
 
-      // ----------- Data Member -----------
-      int    dmID;    			//! Numero identificativo
-      double dmTheta;       //! Angolo ϑ
-      double dmPhi;         //! Angolo φ
-      double dmCDx;         //! Coseno Direttore x.
-      double dmCDy;         //! Coseno Direttore y.
-      double dmCDz;         //! Coseno Direttore z.
-      bool   dmIsrotated;   //! Debug flag.
-
-   ClassDef(Trasporto, 1)
+	ClassDef(Trasporto, 1)
 
 };
 
