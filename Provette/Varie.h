a2cc3764cc964f3f61630a3b888c01e09d401102
Eleonora <eleonora.racca@cern.ch>
2018-08-25 18:37:56 +0200

    funzioni di inclusione di un istogramma e conversione da pseudorapidità in theta

--- Provette/Varie.h
+++ Provette/Varie.h
@@ -1,22 +1,63 @@
-#ifndef Varie_h
-#define Varie_h
-
 /*~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
   ~ Implementazione dell'utility Varie                      ~
   ~ Autori: Racca Eleonora - eleonora.racca288@edu.unito.it ~
   ~         Sauda Cristina - cristina.sauda@edu.unito.it    ~
-  ~ Ultima modifica: 15/11/2017                             ~
+  ~ Ultima modifica: 25/08/2018                             ~
   ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~*/
 
-/*
-typedef Vertice VTX;
+#ifndef Varie_h
+#define Varie_h
 
-struct Vertice {
-  double X;
-  double Y;
-  double Z;
-  int molteplicita;
-};
-*/
+#include "TMath.h"
+
+// ******************************************************************************
+// ************************ Dichiarazione delle funzioni ************************
+// ******************************************************************************
+
+// Funzione che trasforma pseudorapidità in theta
+double EtaTheta(double inferiore, double superiore);
+double EtaTheta(double inferiore, double superiore, TH1F* istogramma);
+
+// Funzione che importa istogrammi
+TH1F* ImportaIstogramma(TString &file, TString &istogramma);
+
+
+
+// ******************************************************************************
+// *********************** Implementazione delle funzioni ***********************
+// ******************************************************************************
+
+double EtaTheta(double inferiore, double superiore, TH1F* istogramma){  
+  double eta;
+  
+  do{
+    eta = istogramma -> GetRandom();
+  }while((eta<0 && eta<=inferiore) || (eta>0 && eta>=superiore));
+  
+  return 2*TMath::ATan(TMath::Exp(-eta));
+}
+
+
+double EtaTheta(double inferiore, double superiore){
+  double eta;
+
+  do{
+    eta = gRandom -> Uniform(inferiore, superiore);
+  }while((eta<0 && eta<=inferiore) || (eta>0 && eta>=superiore));
+  
+  return 2*TMath::ATan(TMath::Exp(-eta));
+}
+
+
+TH1F* ImportaIstogramma(TString &file, TString &istogramma){
+  TFile *fileistogramma = new TFile(file);
+  TH1F *isto = (TH1F*)fileistogramma -> Get(istogramma);
+
+  isto -> SetDirectory(0);
+  fileistogramma -> Close();
+  delete fileistogramma;
+  
+  return isto;
+}
 
 #endif
