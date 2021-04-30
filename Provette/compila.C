9548cd5f3dc399b9a4dd4fe053881b34142f0e02
crisauda <cristina.sauda@edu.unito.it>
2021-04-30 11:43:35 +0200

    Aggiunta di classi incluse al principio del file

--- Provette/compila.C
+++ Provette/compila.C
@@ -2,12 +2,18 @@
   ~ Macro per la compilazione                               ~
   ~ Autori: Racca Eleonora - eleonora.racca288@edu.unito.it ~
   ~         Sauda Cristina - cristina.sauda@edu.unito.it    ~
-  ~ Ultima modifica: 05/09/2018                             ~
+  ~ Ultima modifica: 05/09/2018 
+  ~ Modifiche recenti: 30/04/2021                          ~
   ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~*/
 
 //da eseguire in ~/TANS/Alacre/Provette/$
 //$root -l
 //.x compila.C("force");
+
+#if !defined(__CINT__) || defined(__MAKECINT__)
+#include "TString.h"
+#include "TSystem.h"
+#endif
 
 void compila(TString myopt="fast"){
   TString opt;
