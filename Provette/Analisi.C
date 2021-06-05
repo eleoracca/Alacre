d0c1a66d5db834bb38aa4a2e933ad41d55d051f7
crisauda <cristina.sauda@edu.unito.it>
2021-06-05 17:56:42 +0200

    bozza analisi

--- Provette/Analisi.C
+++ Provette/Analisi.C
@@ -1,5 +1,81 @@
 #include "Riostream.h"
 #include "TF1.h"
-#include "TH1F.h"
+#include "TH1I.h"
 #include "TMath.h"
 #include "Colori.h"
+
+
+void Analisi(){
+
+  // --------------- RICOSTRUZIONE VERTICE---------------
+
+  // funzioni per leggere i tree
+  TFile *fileRicostruzione = new TFile("Output/Ricostruzione.root", "READ");
+  TFile *fileGenerazione = new TFile("Output/Simulazione.root", "READ");
+
+  if(fileRicostruzione->IsZombie()){
+    cout << Avvertimento("Problema nel leggere il file Simulazione.root \nLa ricostruzione si interrompe.") << endl;
+    return kFALSE;
+  }
+  if(fileGenerazione->IsZombie()){
+    cout << Avvertimento("Problema nel creare il file Ricostruzione.root \nLa ricostruzione si interrompe.") << endl;
+    return kFALSE;
+  }
+
+  // lettura ricostruzione
+  fileRicostruzione -> cd();
+  TTree* robinia = (TTree*) fileRicostruzione -> Get("rovere");
+
+  TClonesArray* UrtiRiv1Reco; //= new TClonesArray("Urto", 100);
+  TBranch* Branch1LReco = robinia -> GetBranch("UrtiRivelatore1");
+  Branch1LReco -> SetAddress(&UrtiRiv1Reco);
+
+  TClonesArray* UrtiRiv2Reco; //= new TClonesArray("Urto", 100);
+  TBranch* Branch2LReco = robinia -> GetBranch("UrtiRivelatore2");
+  Branch2LReco -> SetAddress(&UrtiRiv2Reco);
+
+  // lettura generazione
+  fileGenerazione -> cd();
+  TTree* ginko = (TTree*) fileGenerazione -> Get("gaggia");
+  TBranch* BranchVGen = ginko -> GetBranch("Vertice");
+
+  for(int i = 0; i < robinia -> GetEntries(); i++) {
+    robinia -> GetEvent(i);
+    ginko -> GetEvent(i);
+
+    
+  }
+
+  // --------------- GRAFICI ---------------
+  // Grafico della risoluzione dell'apparato
+  // per ogni evento riempio un bin dell'istogramma
+
+  cout << "************** Grafico Risoluzione **************" << endl;
+  TCanvas *cRisoluzioneApp = new TCanvas("cRisoluzioneApp","Risoluzione",200,10,600,400);
+  cRisoluzioneApp->SetFillColor(0);
+  cRisoluzioneApp->cd();
+  TH1I* hRisoluzioneApp = new TH1I("hRisoluzioneApp","Risoluzione", 200,-0.2,0.2); //non ricordo come funzionano
+  hRisoluzioneApp->SetTitle("Risoluzione");
+  hRisoluzioneApp->GetXaxis()->SetTitle("zGen - zReco [cm]");
+  hRisoluzioneApp->GetYaxis()->SetTitle("Conteggi");
+  hRisoluzioneApp->Draw("AP");
+
+  // Grafio della risoluzione in funzione di zGen
+  cout << "************** Grafico Risoluzione vs zGen **************" << endl;
+  TCanvas *cRisoluzioneGen = new TCanvas("cRisoluzioneGen","Risoluzione vs zGen",200,10,600,400);
+  cRisoluzioneGen->SetFillColor(0);
+  cRisoluzioneGen->cd();
+  //TH1I* hRisoluzioneGen = new TH1I("hRisoluzioneGen","Risoluzione", 200,-0.2,0.2); //non ricordo come funzionano
+  //hRisoluzioneGen->SetTitle("Risoluzione");
+  //hRisoluzioneGen->GetXaxis()->SetTitle("zGen [cm]");
+  //hRisoluzioneGen->GetYaxis()->SetTitle("Conteggi");
+  //hRisoluzioneGen->Draw("AP");
+
+  // Grafico efficienza in funzione della molteplicità
+
+  
+  // Grafico risoluzione in funzione della molteplicità
+  // Grafico efficienza in funzione della molteplicità valutando 1sigma
+  // Grafico conteggi per valutare moda del vertice z reco  
+
+}
